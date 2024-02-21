e30ea0a9e8976ca267ae6f2a4cc21f874e067f47
Huaqing Jin <hjin@dev1.wynton.ucsf.edu>
2024-02-20 23:33:25 -0800

    add script for generating new psd

--- python_scripts/gen_psd_data_new.py
+++ python_scripts/gen_psd_data_new.py
@@ -1,49 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This file contains python code to compare with sinica method
-# 
-# It is under the linear setting
-# 
-# Now, I use the same beta from the paper but the PSD as X
-
-# In[5]:
-
-
 import sys
-sys.path.append("../../mypkg")
+sys.path.append("../mypkg")
 
 
 # In[6]:
 
 
 import numpy as np
-import torch
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from easydict import EasyDict as edict
-from tqdm import trange, tqdm
-from pprint import pprint
-import itertools
-from scipy.stats import chi2
-
-
-from constants import DATA_ROOT, RES_ROOT, FIG_ROOT, MIDRES_ROOT
+from tqdm import trange
 from hdf_utils.data_gen import gen_simu_psd_dataset
-from hdf_utils.SIS import SIS_GLIM
-from utils.matrix import col_vec_fn, col_vec2mat_fn, conju_grad, svd_inverse, cholesky_inv
-from utils.functions import logit_fn
-from utils.misc import save_pkl, load_pkl
-from splines import obt_bsp_obasis_Rfn, obt_bsp_basis_Rfn_wrapper
-from projection import euclidean_proj_l1ball
-from optimization.opt import HDFOpt
 from scenarios.simu_linear_psd import settings
-
 from joblib import Parallel, delayed
 
 
+import argparse
+parser = argparse.ArgumentParser(description='gen PSD')
+parser.add_argument('--start', type=int, default=0, help='starting seed') 
+parser.add_argument('--interval', type=int, default=250, help='interval') 
+args = parser.parse_args()
 
+parser.add_argument('--is_std', action='store_true', help='Std PSD across freq or not, if not, only center, no --is_std=False, --is_std=True') 
 np.random.seed(0)
 c = 0.0
 
@@ -53,7 +31,9 @@ data_gen_params.cs = data_gen_params.cs_fn(c)
 data_gen_params.gt_beta = data_gen_params.beta_fn(data_gen_params.cs)
 
 
-data = gen_simu_psd_dataset(n=data_gen_params.n, 
+
+for seed in trange(args.start, args.start+args.interval):
+    gen_simu_psd_dataset(n=data_gen_params.n, 
                             d=data_gen_params.d, 
                             q=data_gen_params.q, 
                             types_=data_gen_params.types_, 
@@ -62,8 +42,7 @@ data = gen_simu_psd_dataset(n=data_gen_params.n,
                             freqs=data_gen_params.freqs, 
                             data_type=data_gen_params.data_type, 
                             data_params=data_gen_params.data_params, 
-                            seed=0, 
+                            seed=seed, 
                             is_std=data_gen_params.is_std, 
                             verbose=1, 
-                            is_gen=False);
-
+                            is_gen=True);
